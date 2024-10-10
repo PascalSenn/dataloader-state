a848da793859b7d7baa564f7ff224720e7460703
PascalSenn <senn.pasc@gmail.com>
2024-10-10 14:34:15 +0200

    Upgrade

--- src/DataLoader.State.GraphQL/DataLoaders.cs
+++ src/DataLoader.State.GraphQL/DataLoaders.cs
@@ -40,30 +40,6 @@ public static class DataLoaders
         IReadOnlyList<int> keys,
         BloggingContext context,
         PagingArguments arguments,
-        CancellationToken cancellationToken) =>
-        await context.Posts
-            .Where(x => keys.Contains(x.BlogId))
-            .OrderBy(x => x.PostId)
-            .ToBatchPageAsync(x => x.BlogId, arguments, cancellationToken);
-
-    [DataLoader]
-    public static async Task<Dictionary<int, Page<Post>>> GetPostsByBlogIdProjectedAsync(
-        IReadOnlyList<int> keys,
-        BloggingContext context,
-        PagingArguments arguments,
-        ISelectorBuilder selector,
-        CancellationToken cancellationToken) =>
-        await context.Posts
-            .Where(x => keys.Contains(x.BlogId))
-            .Select(selector.TryCompile<Post>()!)
-            .OrderBy(x => x.PostId)
-            .ToBatchPageAsync(x => x.BlogId, arguments, cancellationToken);
-
-    [DataLoader]
-    public static async Task<Dictionary<int, Page<Post>>> GetPostsByBlogIdWithFilterAsync(
-        IReadOnlyList<int> keys,
-        BloggingContext context,
-        PagingArguments arguments,
         [DataLoaderState("where")] Expression<Func<Post, bool>> where,
         CancellationToken cancellationToken) =>
         await context.Posts
@@ -71,61 +47,4 @@ public static class DataLoaders
             .Where(where)
             .OrderBy(x => x.PostId)
             .ToBatchPageAsync(x => x.BlogId, arguments, cancellationToken);
-
-    [DataLoader]
-    public static async Task<Dictionary<int, Page<Post>>> GetPostsByBlogIdWithFilterProjectedAsync(
-        IReadOnlyList<int> keys,
-        BloggingContext context,
-        PagingArguments arguments,
-        [DataLoaderState("where")] Expression<Func<Post, bool>> where,
-        ISelectorBuilder selector,
-        CancellationToken cancellationToken) =>
-        await context.Posts
-            .Where(x => keys.Contains(x.BlogId))
-            .Where(where)
-            .Select(selector.TryCompile<Post>()!)
-            .OrderBy(x => x.PostId)
-            .ToBatchPageAsync(x => x.BlogId, arguments, cancellationToken);
-
-    [DataLoader]
-    public static async Task<ILookup<int, Post>> GetPostsFiltered(
-        IReadOnlyList<int> keys,
-        BloggingContext context,
-        [DataLoaderState("where")] Expression<Func<Post, bool>> where,
-        CancellationToken cancellationToken)
-    {
-        var result = await context.Posts
-            .Where(x => keys.Contains(x.BlogId))
-            .Where(where)
-            .OrderBy(x => x.PostId)
-            .ToArrayAsync(cancellationToken);
-
-        return result.ToLookup(x => x.BlogId);
-    }
 }
-
-// public class PersonDataLoaders
-// {
-//     [DataLoader]
-//     public static async Task<Dictionary<Guid, Page<Client>>> PageClientsByApiIdWithFilterAsync(
-//         IReadOnlyList<Guid> keys,
-//         ApiContext context,
-//         PagingArguments arguments,
-//         [DataLoaderState("where")] Expression<Func<Client, bool>> where,
-//         CancellationToken cancellationToken)
-//     {
-//         return await context.Clients
-//             .Where(x => keys.Contains(x.ApiId))
-//             .Where(where)
-//             .OrderBy(x => x.Name)
-//             .ThenBy(x => x.Id)
-//             .ToBatchPageAsync(x => x.ApiId, arguments, cancellationToken);
-//     }
-// }
-// }
-/**
-        var res = await clients.PageClientsByApiIdWithFilter
-            .Where(x => x.Api!.Workspace.Members.Any(y => y.UserId == Guid.Empty))
-            .WithPagingArguments(new PagingArguments(0))
-            .LoadAsync(Guid.Empty, default);
-*/
--- src/DataLoader.State.GraphQL/Query.cs
+++ src/DataLoader.State.GraphQL/Query.cs
@@ -39,92 +39,16 @@ public static class Operations
 public static partial class BlogType
 {
     [UsePaging]
-    public static async Task<Connection<Post>> GetPostsAsync(
+    public static async Task<Connection<Post>> GetGraphQLPosts(
         [Parent("BlogId")] Blog blog,
         IBlogBatchingContext batching,
         PagingArguments arguments,
         CancellationToken cancellationToken)
     {
         return await batching.PostsByBlogId
-            .WithPagingArguments(arguments)
-            .LoadAsync(blog.BlogId, cancellationToken)
-            .ToConnectionAsync();
-    }
-
-    /*
-
-    The projection does not seem to work
-
-    "message": "The LINQ expression 'DbSet<Post>()\n
-            .Where(p => __keys_0\n.Contains(p.BlogId))\n
-            .GroupBy(p => new Post{ \n        PostId = p.PostId, \n        Title = p.Title \n    }\n    .BlogId)'
-            could not be translated.
-            Either rewrite the query in a form that can be translated, or switch to client
-            evaluation explicitly by inserting a call to 'AsEnumerable', 'AsAsyncEnumerable',
-            'ToList', or 'ToListAsync'.
-            See https://go.microsoft.com/fwlink/?linkid=2101038 for more information.",
-    */
-    [UsePaging]
-    public static async Task<Connection<Post>> GetPostsProjectedAndPaged(
-        [Parent("BlogId")] Blog blog,
-        IBlogBatchingContext batching,
-        PagingArguments arguments,
-        ISelection selection,
-        CancellationToken cancellationToken)
-    {
-        return await batching.PostsByBlogIdProjected
-            .WithPagingArguments(arguments)
-            .Select(selection)
-            .LoadAsync(blog.BlogId, cancellationToken)
-            .ToConnectionAsync();
-    }
-
-    /**
-    This is does not work because the context data is not porpagated inside of the paging
-    dataloader
-
-    "message": "The state `where` is not available on the DataLoader.",
-
-    */
-    [UsePaging]
-    public static async Task<Connection<Post>> GetPostsFilteredAndPaged(
-        [Parent("BlogId")] Blog blog,
-        IBlogBatchingContext batching,
-        PagingArguments arguments,
-        CancellationToken cancellationToken)
-    {
-        return await batching.PostsByBlogIdWithFilter
             .Where(x => x.Title.Contains("GraphQL"))
             .WithPagingArguments(arguments)
             .LoadAsync(blog.BlogId, cancellationToken)
             .ToConnectionAsync();
-    }
-
-    /// Also does not work
-    [UsePaging]
-    public static async Task<Connection<Post>> GetPostsFilteredAndProjectedAndPaged(
-        [Parent("BlogId")] Blog blog,
-        IBlogBatchingContext batching,
-        PagingArguments arguments,
-        ISelection selection,
-        CancellationToken cancellationToken)
-    {
-        return await batching.PostsByBlogIdWithFilterProjected
-            .Where(x => x.Title.Contains("GraphQL"))
-            .WithPagingArguments(arguments)
-            .Select(selection)
-            .LoadAsync(blog.BlogId, cancellationToken)
-            .ToConnectionAsync();
-    }
-
-    /// This works
-    public static async Task<Post[]?> GetPostsFiltered(
-        [Parent("BlogId")] Blog blog,
-        IBlogBatchingContext batching,
-        CancellationToken cancellationToken)
-    {
-        return await batching.PostsFiltered
-            .Where(x => x.Title.Contains("GraphQL"))
-            .LoadAsync(blog.BlogId, cancellationToken);
     }
 }
